737cf1d14b45212683ea05659ef1fb9e36e1d374
accfacebook2712-cmd <accfacebook2712@gmail.com>
2026-03-17 22:07:26 +0700

    Create Dessert.java file in model package

---
+++ src/com/restaurant/model/Dessert.java
@@ -0,0 +1,17 @@
+public class Dessert {
+    private String name;
+    private double price;
+
+    public Dessert(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
